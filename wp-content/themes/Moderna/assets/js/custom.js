3305b08b82d9c817d8c19a59f65cd28d03ff300a
webmaster <paharenko1985@mail.ru>
2018-03-07 13:21:15 +0200

    add filter

--- wp-content/themes/Moderna/assets/js/custom.js
+++ wp-content/themes/Moderna/assets/js/custom.js
@@ -135,3 +135,22 @@ jQuery(document).ready(function($) {
     });
 
 });
+
+jQuery(function($){
+	$('#filter').submit(function(){
+		var filter = $(this);
+		$.ajax({
+			url:ajaxurl, // обработчик
+			data:filter.serialize(), // данные
+			type:filter.attr('method'), // тип запроса
+			beforeSend:function(xhr){
+				filter.find('button').text('Загружаю...'); // изменяем текст кнопки
+			},
+			success:function(data){
+				filter.find('button').text('Применить фильтр'); // возвращаеи текст кнопки
+				$('#response').html(data);
+			}
+		});
+		return false;
+	});
+});
--- wp-content/themes/Moderna/functions.php
+++ wp-content/themes/Moderna/functions.php
@@ -85,45 +85,40 @@ function register_my_widget() {
 add_action( 'widgets_init', 'register_my_widget' );
 
 
-// ---------------------------------------------------------------- //
-
-//    $url = 'http://www.cbr.ru/scripts/XML_daily.asp';
-//    $expiration = DAY_IN_SECONDS / 2;
-//    $resp = wp_remote_get( $url );
-//
-//    if ( wp_remote_retrieve_response_code($resp) === 200 ) {
-//
-//        $body = wp_remote_retrieve_body( $resp );
-//
-//        $xml = simplexml_load_string($body);
-//        $data = json_decode(json_encode($xml));
-//
-//        $USD = wp_list_filter( $data->Valute, array('CharCode'=>'USD') );
-//        $USD = array_shift($USD);
-//
-//        $UAH = wp_list_filter( $data->Valute, array('CharCode'=>'UAH') );
-//        $UAH = array_shift($UAH);
-//
-//        $EUR = wp_list_filter( $data->Valute, array('CharCode'=>'EUR') );
-//        $EUR = array_shift($EUR);
-//
-//
-//        $usd_in_rub = $USD->Value;
-//        $usd_in_uah = $UAH->Value;
-//        $usd_in_eur = $EUR->Value;
-//
-//}
-//
-//echo '<h4>Курс доллара к рублю на сегодня: $1 = '. $usd_in_rub .' руб.</h4>';
-//echo '<h4>Курс доллара к гривне на сегодня: $1 = '. $usd_in_uah .' руб.</h4>';
-//echo '<h4>Курс доллара к Евро на сегодня: $1 = '. $usd_in_eur .' руб.</h4>';
 
 
-// ---------------------------------------------------------------- //
+function true_filter_function(){
 
-//$resp = wp_remote_get( 'http://api.apixu.com/v1/current.json?key=ed4694f8f29a49e4b52122351180503&q=Kharkiv' );
-//var_dump($resp['body']);
-//$res = json_decode($resp['body']);
-//
-//echo "Температура воздуха в городе <b>{$res->location->name}</b> сейчас <b>{$res->current->temp_c}</b>  градусов";
+	$args = array(
+		'orderby' => 'date', // сортировка по дате у нас будет в любом случае (но вы можете изменить/доработать это)
+		'order'	=> $_POST['date'] // ASC или DESC
+	);
+ 
+	// для таксономий
+
+		$args['tax_query'] = array(
+			array(
+				'taxonomy' => 'category',
+				'field' => 'id',
+				'terms' => $_POST['categoryfilter']
+			)
+		);
+
+	// условие 3: миниатюра имеется
+	if( isset( $_POST['featured_image'] ) && $_POST['featured_image'] == 'on' )
+		$args['meta_query'][] = array(
+			'key' => '_thumbnail_id',
+			'compare' => 'EXISTS'
+		);
+ 
+	die();
+}
+ 
+ 
+add_action('wp_ajax_myfilter', 'true_filter_function'); 
+add_action('wp_ajax_nopriv_myfilter', 'true_filter_function');
+
+
+
+
 
